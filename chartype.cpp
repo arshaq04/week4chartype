68bc63c73ebad26453fbfc400b44c6dc00696766
Arshaq Ahmed <arshahm2004@gmail.com>
2023-11-07 00:05:28 +0000

    cctype function

--- chartype.cpp
+++ chartype.cpp
@@ -10,21 +10,20 @@ int main(){
     cout << "Enter a character: " << endl;
     cin >> x;
 
-    if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {   
-    cout << x << " is a vowel" << endl;
-    }
-    if (x == 'b') {   
-    cout << x << " is a consonant" << endl;
-    }
-    if (x == 1) {   
-    cout << x << " is a digit" << endl;
-    }
-    if (x == ',') {   
-    cout << x << " is a puntuation" << endl;
-    }
-    if (x == '£') {   
-    cout << x << " is not recognised" << endl;
-    }
+    if (isalpha(x)) { //isalpha checks whether the character is in the alphabet or not
+        char bigX = tolower(x); /* if I did char x == a, e, i, o, u it wouldn't read the uppercase versions of the vowels as a vowel. 
+        In order for it to read the uppercase letters as vowels too, we can do tolower which converts any uppercase letter to lowercase then compares the 
+        input to see if it satisfies the condition. */
+        if (bigX == 'a' || bigX == 'e' || bigX == 'i' || bigX == 'o' || bigX == 'u') {
+            cout << x << " is a vowel" << endl; }
+        else {
+            cout << x << " is a consonant" << endl; } 
+        } else if (isdigit(x)) { //checks if x is a digit
+            cout << x << " is a digit" << endl; }
+        else if (ispunct(x)) { //checks if x is a punctuation
+            cout << x << " is a punctuation" << endl; }
+        else {
+            cout << x << " is not recognised" << endl; }
 
 return 0;
 }
